92c725dbf4eec2638053ddb697137abef284f903
Kirill Ki <kirillkalapov@gmail.com>
2022-02-18 16:47:46 +0100

    Initial Commit

---
+++ SportMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportMVP
+//
+//  Created by  Mr.Ki on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
